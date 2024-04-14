58eac88809aa517fcc57c76ad92a7639a3a64190
wildpjah <wildpjah@gmail.com>
2024-04-14 00:50:26 -0500

    game object and WIP finding new room

--- objects/Miner.py
+++ objects/Miner.py
@@ -1,4 +1,10 @@
+from .Game import Game
 from .Person import Person
 class Miner(Person):
-    def __init__(self, id, name, coins=0, room=None):
-        super().__init__(id, name, coins, room)
+    def __init__(self, game, id, name, coins=0, room=None):
+        super().__init__(game, id, name, coins, room)
+
+    def enter_room(self, room):
+        self.room = room
+    def find_room(self):
+        options = self.game.get_un_occ
